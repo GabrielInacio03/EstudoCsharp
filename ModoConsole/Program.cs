6a8dccf4aebb6b5761047152f83fa66cfd61b182
GabrielInacio03 <inaciogs03@gmail.com>
2022-06-15 08:31:02 -0300

    primeiro commit

---
+++ ModoConsole/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ModoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("C# em Modo Console");
+            Console.WriteLine("-------------------------");
+
+            // Tipos de dados (Principais)
+            /*
+                int -> inteiros
+                string -> caracteres
+                float,double -> números com casa decimais
+                boolean -> verdadeiro ou falso                
+            */
+
+            //exemplos                        
+            bool menino = true;
+
+            Console.WriteLine("Informe seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Informe sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            
+            Console.WriteLine("Informe seu peso: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            string msgSexo = (menino == true) ? "Menino" : "Menina";
+
+            string msg = "Meu nome é "+ nome +", tenho "+ idade +" anos e peso "+ peso +"kg. Sou "+ msgSexo;            
+
+            Console.WriteLine("Apresentação: ");
+            Console.WriteLine(msg);           
+            Console.ReadKey();
+        }
+    }
+}
